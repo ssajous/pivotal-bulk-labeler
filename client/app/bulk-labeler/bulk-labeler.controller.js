96bd8225469d348956d1e30352e09d8666b4e18f
Stan Sajous <stan.sajous@gmail.com>
2015-03-17 15:54:45 -0500

    minor cleanup of filter management

--- client/app/bulk-labeler/bulk-labeler.controller.js
+++ client/app/bulk-labeler/bulk-labeler.controller.js
@@ -20,6 +20,15 @@ angular.module('pivotalUtilsApp')
       };
 
       $scope.stateSelected = function() {
+        var stateString = 'with_state=' + $scope.selectedState;
+
+        if (_($scope.filters).find(function(filter) { return filter === stateString; })) {
+          return; // existing filter, nothing needed.
+        }
+        $scope.filters = _($scope.filters).reject(function(filter) {
+          return filter.search('with_state=') > -1;
+        }).value();
+
         $scope.filters.push('with_state=' + $scope.selectedState);
         console.log($scope.filters);
 
